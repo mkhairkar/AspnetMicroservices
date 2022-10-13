0ca76967cf984d139df436036370e8532e64d32f
mkhairkar <mayurkhairkar66@gmail.com>
2022-10-13 17:49:53 +0530

    commit

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrusructure/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Infrusructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
